c7b2fd2d9db17c15a2d18ecb7f7c14bff60fa52c
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 16:13:03 +0700

    Create the project

---
+++ Glassmorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glassmorphism
+//
+//  Created by Danh Tu on 05/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by Danh Tu on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
