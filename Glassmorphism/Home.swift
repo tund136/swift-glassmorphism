50ed7ccd18e067b34271d4b96a5824551782ae65
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-05 16:34:06 +0700

    Gloss Background

--- Glassmorphism/Home.swift
+++ Glassmorphism/Home.swift
@@ -17,6 +17,52 @@ struct Home: View {
             )
                 .ignoresSafeArea()
             
+            // Gloss Background
+            GeometryReader { proxy in
+                let size = proxy.size
+                
+                // Slighty Darkening
+                Color.black
+                    .opacity(0.7)
+                    .blur(radius: 200)
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(50)
+                    .blur(radius: 120)
+                // Moving top
+                    .offset(x: -size.width / 1.8, y: -size.height / 5)
+                
+                Circle()
+                    .fill(Color("LightBlue"))
+                    .padding(50)
+                    .blur(radius: 150)
+                // Moving top
+                    .offset(x: size.width / 1.8, y: -size.height / 2)
+                
+                Circle()
+                    .fill(Color("LightBlue"))
+                    .padding(50)
+                    .blur(radius: 90)
+                // Moving bottom
+                    .offset(x: size.width / 1.8, y: size.height / 2)
+                
+                // Adding Purple on both bottom ends
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(100)
+                    .blur(radius: 110)
+                // Moving bottom
+                    .offset(x: size.width / 1.8, y: size.height / 2)
+                
+                Circle()
+                    .fill(Color("Purple"))
+                    .padding(100)
+                    .blur(radius: 110)
+                // Moving bottom
+                    .offset(x: -size.width / 1.8, y: size.height / 2)
+            }
         }
     }
 }
